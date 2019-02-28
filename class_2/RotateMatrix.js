5ba1d9e9dd11ceef477828bfa7dc3326c5da7c68
andrefmrocha <andrefmrocha@live.com.pt>
2019-02-28 15:51:31 +0000

    Finished all rotation functions

--- class_2/RotateMatrix.js
+++ class_2/RotateMatrix.js
@@ -9,3 +9,31 @@ function rotate_Z(ang){
     ];
     return m;
 }
+
+function rotate_X(ang){
+    var radian  = ang * Math.PI/180;
+
+    m = [
+        1, 0, 0, 0,
+        0, Math.cos(radian), Math.sen(radian), 0,
+        0, -Math.sin(radian), Math.cos(radian), 0,
+        0, 0, 0, 1
+    ];
+    return m;
+}
+
+
+function rotate_Y(ang){
+    var radian  = ang * Math.PI/180;
+
+    m = [
+        Math.cos(radian), 0, -Math.sin(radian), 0,
+        0, 1, 0, 0,
+        Math.sin(radian), 0, Math.cos(radian), 0,
+        0, 0, 0, 1
+    ];
+    return m;
+}
+
+
+
