23481bf0aa1428d0d3bfd413907211f32cb8b32f
Gustavo Torres <torresgustam@gmail.com>
2019-03-20 20:04:25 +0000

    Minecraft cube done.

--- class_4/MyUnitCubeQuad.js
+++ class_4/MyUnitCubeQuad.js
@@ -4,10 +4,12 @@ class MyUnitCubeQuad extends CGFobject{
         this.quad = new MyQuad(scene);
     }
     display(){
+        this.scene.materialMineSide.apply();
         this.scene.pushMatrix();
         this.scene.scale(0.5, 0.5, 0.5);
         this.scene.pushMatrix();
         this.scene.translate(0, 0, 0.5);
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
         this.quad.display();
         this.scene.popMatrix();
         this.scene.pushMatrix();
@@ -24,11 +26,15 @@ class MyUnitCubeQuad extends CGFobject{
         this.scene.translate(0, 0, -0.5);
         this.scene.rotate(Math.PI, 0, 1, 0);
         this.quad.display();
+        this.scene.materialMineTop.apply();
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
         this.scene.popMatrix();
         this.scene.pushMatrix();
         this.scene.translate(0, 0.5, 0);
         this.scene.rotate(-Math.PI/2, 1, 0, 0);
         this.quad.display();
+        this.scene.materialMineBottom.apply();
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
         this.scene.popMatrix();
         this.scene.pushMatrix();
         this.scene.translate(0, -0.5, 0);
